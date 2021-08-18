66f70dd8d7e525a511880bd85891b8218d61bbfd
Klodian Lala <k.lala@lalatech.de>
2021-08-18 15:38:04 +0200

    first example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,147 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main>
+    <div>
+      <div >
+        <div >
+          <input
+            id="height"
+            type="number"
+            v-model="valHeight[0]"
+            @input="heightOnePlate(valHeight[0])"
+            placeholder="3000"
+            min="100"
+            max="3000"
+          />
+        </div>
+      </div>
+      <div  >
+        <div v-for="(item, index) in [1,2,3]" :key="index">
+          <div>
+            <input
+              id="width"
+              type="number"
+              v-model="valWidth[index]"
+              @input="widthMultiplePlates(index, valWidth[index])"
+              placeholder="1500"
+              min="100"
+              max="1500"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <cropper
+      class="cropper"
+      boundingBoxClass="main-photo"
+      :src="require('./DE17-02.jpg')"
+      :default-size="defaultSize"
+      minWidth="20"
+      minHeight="20"
+      :stencil-props="{
+        aspectRatio: aspectRatio,
+      }"
+      @change="change"
+    ></cropper>
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+import {
+  defineComponent,
+  computed,
+  ref,
+  reactive,
+} from "vue";
+import { Cropper } from "vue-advanced-cropper";
+import "vue-advanced-cropper/dist/style.css";
+// import Stencil from "./cropper/Stencil.vue";
+export default defineComponent({
+  components: {
+    Cropper,
+    // eslint-disable-next-line vue/no-unused-components
+    // Stencil,
+  },
+  data() {
+    return {
+    };
+  },
+  setup() {
+    const valWidth = reactive([undefined, undefined, undefined]);
+    const valHeight = reactive([]);
+
+    const platesNumberWidth = ref([1500, 0, 0]);
+    const platesNumberHeight = ref(3000);
+    const imagePlacement = reactive({
+      width : 0,
+      height: 0,
+      crop: {
+        width : 0,
+        height: 0,
+        left  : 0,
+        top   : 0,
+      }
+    })
+    // function limit(number, min, max)  {
+    //   if(number < min) return min;
+    //   return number > max ? max : number;
+    // }
+    // function ifNotValueThenMax(number, max){
+    //   if(!number) return max
+    //   return Number(number);
+    // }
+    const heightOnePlate = (val) => {
+      valHeight[0] =  Number(val) || undefined;
+      platesNumberHeight.value = val;
+    };
+    // width
+    let widthMultiplePlates = (index, val) => {
+      valWidth[index] = Number(val) || undefined;
+      platesNumberWidth.value[index] = val;
+    };
+    const change = ({ image }) => {
+      imagePlacement.width = image.width;
+      imagePlacement.height = image.height;
+    };
+
+    const neededHeight = computed(() => {
+      return platesNumberHeight.value / 10;
+    })
+    const neededWidth = computed(() => {
+      return platesNumberWidth.value.reduce((acc, inc) => acc + inc, 0) / 10;
+    })
+
+    const aspectRatio = computed(() => {
+      return String(neededWidth.value / neededHeight.value);
+    });
+
+    const defaultSize = computed(() =>  {
+      let aspectRatio = Number(neededWidth.value/neededHeight.value) || 1;
+      if (aspectRatio > imagePlacement.width / imagePlacement.height) {
+        return {
+          width: imagePlacement.width,
+          height: imagePlacement.width / aspectRatio,
+        };
+      } else {
+        return {
+          width: imagePlacement.height * aspectRatio,
+          height: imagePlacement.height,
+        };
+      }
+    })
+    return {
+      defaultSize,
+      change,
+      heightOnePlate,
+      widthMultiplePlates,
+      platesNumberWidth,
+      platesNumberHeight,
+      valWidth,
+      valHeight,
+      aspectRatio,
+    };
+  },
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
 </style>
